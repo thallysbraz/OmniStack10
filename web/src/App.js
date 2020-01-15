3829c34be48b6acc1b8e93e93459be9816783c79
thallysbraz <thallysbraz3@gmail.com>
2020-01-15 09:10:59 -0300

    adicionando CSS global

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./global.css";
+
+// Componente
+// Estado
+// Propriedade
+
+// Componente: Bloco isolado de HTML, CSS E JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI pasa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div id="app">
+      <aside></aside>
+      <main></main>
     </div>
   );
 }
